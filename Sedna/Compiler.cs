745ea84dab04295ff27321a3fd007a7e716bf7e6
Myvar <myvar@outlook.com>
2016-10-17 09:11:07 +0200

    Added sedna parser

---
+++ Sedna/Compiler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sedna.Core.Internals;
+using Sedna.Core.Internals.Ast;
+using System.Collections;
+
+namespace Sedna.Core
+{
+    public class Compiler
+    {
+        private static CompilerScope _Scope  = new CompilerScope();
+
+        public Compiler()
+        {
+            
+        }
+
+        public void Compile(List<string> src, string Output, string target)
+        {
+            /*
+                Steps:
+                1. Tokenize
+                2. Parse
+                3. Astyfie
+                4. Assemble
+            */
+            _Scope = new CompilerScope();
+           
+            foreach (var i in src)
+            {
+                var x = Parser.Parse(File.ReadAllText(i));
+
+                if (!CheckErrors()) return;
+                //build byte code
+            }
+
+           
+        }
+
+        public static  bool CheckErrors()
+        {
+            if (Parser.Errors.Count == 0 && _Scope.Errors.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Found Errors:");
+                foreach (var i in Parser.Errors)
+                {
+                    Console.WriteLine(i);
+                }
+
+                foreach (var i in _Scope.Errors)
+                {
+                    Console.WriteLine(i);
+                }
+                return false;
+            }
+        }  
+    }
+}
---
+++ Sedna/Internals/Ast/InvokeStmt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sedna.Core.Internals.Ast
+{
+    public class InvokeStmt : IAst
+    {
+        public string Path { get; set; }
+        public List<IAst> Perams { get; set; } = new List<IAst>();
+
+        public override bool IsValid(Token raw)
+        {
+            if(string.IsNullOrEmpty(raw.Raw))
+            {
+                return false;
+            }
+            if (char.IsDigit(raw.Raw[0]))
+            {
+                return false;
+            }
+            if (raw.Raw.Trim().EndsWith(")", StringComparison.Ordinal) && 
+                !raw.Raw.Trim().StartsWith("(", StringComparison.Ordinal))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override IAst Parse(Token raw)
+        {
+            var re = new InvokeStmt();
+
+            re.Path = raw.Raw.Trim().Split('(')[0];
+            foreach (var i in raw.Raw.Trim().Remove(raw.Raw.Trim().Length - 1, 1).Replace(re.Path, "").Remove(0,1).Split(','))
+            {
+                var x = IAst.ParseToken(new Token() { Raw = i.Trim() });
+                if(x != null)
+                {
+                    re.Perams.AddRange(x);
+                }
+            }
+
+            return re;
+        }
+    }
+}
